5d9559d0e49de59cc9b9a367712da9e2644be4df
danielzuhad <danielzuhad@gmail.com>
2024-05-07 14:20:10 +0700

    sdf

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,9 +5,10 @@ const Login = async () => {
     <>
       <div className="flex h-full w-full items-center justify-center pb-36">
         <div className="flex flex-col items-center ">
-          <h1 className="mb-20 p-2 text-center text-6xl font-semibold max-sm:text-5xl">
+          <h1 className=" p-2 text-center text-6xl font-semibold max-sm:text-5xl">
             Spotify Clone
           </h1>
+          <h2 className="mb-20 mt-5 text-[#b1b1b1]">(Premium only)</h2>
 
           <LoginButtonsSection />
         </div>
